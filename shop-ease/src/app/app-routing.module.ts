8e058cb5a894ab7fe92c0ed618f8a0005c236c84
EdsonJm <edsonjm99@gmail.com>
2025-03-22 20:02:23 -0600

    modules and routing config

--- shop-ease/src/app/app-routing.module.ts
+++ shop-ease/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./user-management/user-management.module').then(m => m.UserManagementModule)
+  },
+  {
+    path: 'catalog',
+    loadChildren: () =>
+      import('./catalog/catalog.module').then(m => m.CatalogModule)
+  },
+  {
+    path: 'shopping-cart',
+    loadChildren: () =>
+      import('./shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)
+  },
+  {
+    path: 'payment',
+    loadChildren: () =>
+      import('./payment/payment.module').then(m => m.PaymentModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
