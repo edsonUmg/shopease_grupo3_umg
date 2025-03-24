79e407189dc6aec1f955ff1f6edd82f477a5bd93
Marco-and <macv203@gmail.com>
2025-03-24 15:40:24 -0600

    Backup de mis cambios locales en feature/initialConfig

--- shop-ease/src/app/catalog/catalog.component.ts
+++ shop-ease/src/app/catalog/catalog.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.scss']
+  styleUrls: ['./catalog.component.scss'],
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
