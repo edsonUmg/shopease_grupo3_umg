6c333a7e40b3360c1473f8d82148027dc6dad0aa
Marco-and <macv203@gmail.com>
2025-03-25 23:33:06 -0600

    Agregado soporte para imágenes externas en el catálogo

--- shop-ease/src/app/catalog/catalog.component.ts
+++ shop-ease/src/app/catalog/catalog.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.scss']
+  styleUrls: ['./catalog.component.scss'],
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  productos: Product[] = [];
+  productosFiltrados: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.productos = data;
+      this.productosFiltrados = data;
+    });
+  }
+
+  filtrarPorDisponibilidad(event: any): void {
+    if (event.target.checked) {
+      this.productosFiltrados = this.productos.filter((p) => p.available);
+    } else {
+      this.productosFiltrados = [...this.productos];
+    }
+  }
+
+  agregarAlCarrito(producto: Product): void {
+    alert(`${producto.name} agregado al carrito`);
+  }
 }
