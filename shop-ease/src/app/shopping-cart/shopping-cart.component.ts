97d42d14c3e8f6029fafd6c2f9ea6a9baf457cc1
EdsonJm <edsonjm99@gmail.com>
2025-03-29 16:17:59 -0600

    add button for redirect to payment page

--- shop-ease/src/app/shopping-cart/shopping-cart.component.ts
+++ shop-ease/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
 import { Product } from '../models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.scss']
 })
-export class ShoppingCartComponent implements OnInit{
+export class ShoppingCartComponent implements OnInit {
   carrito: Product[] = [];
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
     this.carrito = this.cartService.obtenerCarrito();
@@ -19,6 +20,10 @@ export class ShoppingCartComponent implements OnInit{
   limpiarCarrito(): void {
     this.cartService.limpiarCarrito();
     this.carrito = [];
+  }
+
+  irAPago(): void {
+    this.router.navigate(['/payment']);
   }
 
 }
