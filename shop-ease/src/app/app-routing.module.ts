51f58f1e58cb64d7588054d19f94fa8b7ca0a12f
2103117 <73920649+2103117@users.noreply.github.com>
2025-04-07 22:19:13 -0600

    Fixes para la ejecución y ajuste de estilos del login

--- shop-ease/src/app/app-routing.module.ts
+++ shop-ease/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { AuthGuard } from '@app/guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: 'user',
+    path: 'user-management',
     loadChildren: () =>
       import('./user-management/user-management.module').then(m => m.UserManagementModule)
   },
--- shop-ease/src/app/app.module.ts
+++ shop-ease/src/app/app.module.ts
@@ -6,12 +6,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { UserManagementModule } from './user-management/user-management.module';
-import { ConfirmDialogComponent } from './shared/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
